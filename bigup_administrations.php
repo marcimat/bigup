e0cd8bf6fd31fd21ac9cc939a4f040972e30df82
Matthieu Marcillaud <marcimat@rezo.net>
2015-11-10 20:24:21 +0100

    Création de la configuration à l'installation.

---
+++ bigup_administrations.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Fichier gérant l'installation et désinstallation du plugin Big Upload
+ *
+ * @plugin     Big Upload
+ * @copyright  2015
+ * @author     marcimat
+ * @licence    GNU/GPL
+ * @package    SPIP\Bigup\Installation
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/**
+ * Fonction d'installation et de mise à jour du plugin Big Upload.
+ * 
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @param string $version_cible
+ *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ * @return void
+**/
+function bigup_upgrade($nom_meta_base_version, $version_cible) {
+	$maj = array();
+
+	// Configuration par défaut
+	$config_defaut = [
+		'max_file_size' => 5, // 5 Mb par défaut
+	];
+
+	$maj['create'] = [['ecrire_meta', 'bigup', serialize($config_defaut)]];
+
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+
+/**
+ * Fonction de désinstallation du plugin Big Upload.
+ * 
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @return void
+**/
+function bigup_vider_tables($nom_meta_base_version) {
+	effacer_meta($nom_meta_base_version);
+}
