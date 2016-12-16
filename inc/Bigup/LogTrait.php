0d47c0f90c5ec4eb78aba5be5e0b6285a5ca77e8
Matthieu Marcillaud <marcimat@rezo.net>
2016-12-16 16:05:53 +0100

    Permettre d'utiliser les logs dans des méthodes statiques.

--- inc/Bigup/LogTrait.php
+++ inc/Bigup/LogTrait.php
@@ -22,19 +22,19 @@ trait LogTrait {
 	 * @param mixed $quoi
 	 * @param gravite $quoi
 	**/
-	public function log($quoi, $gravite = _LOG_INFO_IMPORTANTE) {
+	public static function log($quoi, $gravite = _LOG_INFO_IMPORTANTE) {
 		spip_log($quoi, "bigup." . $gravite);
 	}
 
-	public function debug($quoi) {
-		return $this->log($quoi, _LOG_DEBUG);
+	public static function debug($quoi) {
+		return self::log($quoi, _LOG_DEBUG);
 	}
 
-	public function error($quoi) {
-		return $this->log($quoi, _LOG_ERREUR);
+	public static function error($quoi) {
+		return self::log($quoi, _LOG_ERREUR);
 	}
 
-	public function info($quoi) {
-		return $this->log($quoi, _LOG_INFO);
+	public static function info($quoi) {
+		return self::log($quoi, _LOG_INFO);
 	}
 }
